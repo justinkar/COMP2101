b28564c51e59f1715fd2193548a46ede98fbbc7f
Justin Karjalainen <justinkarj@gmail.com>
2022-06-09 08:24:38 -0400

    revised sysinfo lab 2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Lab 1 -  sysinfo.sh
+# Lab 2 - cleaning up the sysinfo.sh
 
-# This is a script with various commands to display the system information of my virtual machine 
+# This is a script with various commands to display the system information of my virtual machine and making it easier to read 
 
-# Display hostname
-echo -n " my Host name is: "
-hostname
+# Title
 
-# DNS diplay
-echo -n " my DNS is: "
-hostname -d |
+echo -n " This is a report for myvm: "
+
+# Display Full Qualified Domain name
+echo -n " my FQDN is: "
+hostname --fqdn
 
 # Operating system
 echo -n " Operating System name and version: "
-grep "$NAME" /etc/os-release
+egrep '^(VERSION)=' /etc/os-release
 
 # Display IP addresses
-echo -n " My IP addresses are: "
-ip a s ens33 | awk '/inet|inet6 /{print $2}'
+echo -n " My IP address is: "
+ip a s ens33 | awk '/inet /{print $2}'
 
 # Show space on disk
-echo -n " Root File System information: "
+echo -n " Root File System Freespace: "
 df -T -h /dev/sda1 
